634b1c10829bf2df7c506dc1425baa695f7bf85b
Oleksandr <oleksandrko448@gmail.com>
2023-06-26 05:05:32 +0300

    init chrome extension

---
+++ content.js
@@ -0,0 +1,28 @@
+function executeFunction() {
+    // Place your function code here
+    console.log("Executing function...");
+}
+
+function getRandomInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function executeAtRandomInterval() {
+    // Generate a random time interval between 1 and 5 seconds
+    var interval = getRandomInterval(1000, 10000);
+
+    // Wait for the random interval
+    setTimeout(function () {
+        // Call the function
+        executeFunction();
+
+        // Call the executeAtRandomInterval function again
+        executeAtRandomInterval();
+    }, interval);
+}
+
+var url = window.location.href
+if(url.includes("openai")) {
+    // Start the execution
+    executeAtRandomInterval();
+}
